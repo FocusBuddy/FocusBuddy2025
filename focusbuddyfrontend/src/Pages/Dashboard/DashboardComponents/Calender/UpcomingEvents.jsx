7e11a84d2ff21f44b2d1bdae761aa6d85b45ae5b
r1414a <rupeshchincholkar14@gmail.com>
2024-10-15 18:09:53 +0530

    session issues

--- focusbuddyfrontend/src/Pages/Dashboard/DashboardComponents/Calender/UpcomingEvents.jsx
+++ focusbuddyfrontend/src/Pages/Dashboard/DashboardComponents/Calender/UpcomingEvents.jsx
@@ -187,7 +187,7 @@ export default function UpcomingEvents({
       </div>
 
       <div style={{zIndex:3002}} id="joinbuttontooltip" className="flex mt-4 justify-between">
-        <Tooltip className={`max-w-[16rem] text-center ${showJoin ? 'hidden': 'block'}`} content="You can join your session 10 minutes before it starts">
+        {/* <Tooltip className={`max-w-[16rem] text-center ${showJoin ? 'hidden': 'block'}`} content="You can join your session 10 minutes before it starts">
         <button type="button" className="flex items-center gap-1 px-1 md:px-2 font-semibold bg-white text-textcolor border-2 border-bordercolor hover:bg-gray-200">
         <Link
           to={`/sessions/${availableEvents[0].callID}`}
@@ -201,8 +201,8 @@ export default function UpcomingEvents({
           <GoArrowRight /> Join
         </Link>
         </button>
-           </Tooltip>
-        {/* <Tooltip className={`max-w-[16rem] text-center ${showJoin ? 'hidden': 'block'}`} content="You can join your session 10 minutes before it starts">
+           </Tooltip> */}
+        <Tooltip className={`max-w-[16rem] text-center ${showJoin ? 'hidden': 'block'}`} content="You can join your session 10 minutes before it starts">
         <button type="button" className="flex items-center gap-1 px-1 md:px-2 font-semibold bg-white text-textcolor border-2 border-bordercolor hover:bg-gray-200">
         <Link
           to={`/sessions/${availableEvents[0].callID}`}
@@ -216,7 +216,7 @@ export default function UpcomingEvents({
           <GoArrowRight /> Join
         </Link>
         </button>
-           </Tooltip> */}
+           </Tooltip>
         <div className="flex gap-2 items-center">
           <span>
             {showJoin === true && startEvent === false ? (
