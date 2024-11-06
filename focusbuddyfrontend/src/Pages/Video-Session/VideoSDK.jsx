63c0d7267d57c83f2c574c8f049734a88741ce73
r1414a <rupeshchincholkar14@gmail.com>
2024-11-06 19:43:38 +0530

    video changes

--- focusbuddyfrontend/src/Pages/Video-Session/VideoSDK.jsx
+++ focusbuddyfrontend/src/Pages/Video-Session/VideoSDK.jsx
@@ -158,7 +158,7 @@ export const MyUILayout = (props) => {
   }
 
   return (
-    <div className='px-0 bg-[#222222] min-h-screen'>
+    <div className='px-0 bg-[#222222] min-h-screen overflow-hidden'>
     <ParticipantsState 
        call={props.call} 
        availableEvents={props.availableEvents}/>
